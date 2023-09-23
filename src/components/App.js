4092922aab07e11cca7971a8ffa81b33edfe3eff
NiketSah <117289451+NiketSah@users.noreply.github.com>
2023-09-23 23:30:06 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,17 @@ import React from 'react'
 import '../styles/App.css';
 import styles from '../styles/appStyles.module.css';
 const App = () => {
-const heading ={
+const inlineStyles ={
   fontsize: '50px',
   color: red,
 };
   return (
     <div id="main">
       <h1  className={styles.success}>Success</h1>
-      <h1 id='error' style={heading}>Error</h1>
+      <h1 id='error' style={inlineStyles}>Error</h1>
     </div>
-  )
-}
+  );
+};
 
 
 export default App;
