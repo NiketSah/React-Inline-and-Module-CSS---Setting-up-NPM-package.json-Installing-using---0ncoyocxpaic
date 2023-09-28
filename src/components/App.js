7004d19a11d62f1bddc90bb167dd9673ed82525a
NiketSah <sahniket15@gmail.com>
2023-09-28 20:30:39 +0530

    include code change for error

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,12 @@ import React from 'react';
 import styles from '../styles/appStyles.module.css';
 
 const App = () => {
-  const headingStyle = {
-    fontSize: '50px',
-    color: 'red',
-  };
+
 
   return (
     <div id="main">
       <h1 className={styles.success}>Success</h1>
-      <h1 style={headingStyle}>Error</h1>
+      <h1 id={styles.error}>Error</h1>
     </div>
   );
 }
